576215ef05a585dc8f48b07714f495f0b709bc5e
Jagoda Blaszkiewicz <jagabla8@gmail.com>
2022-10-02 01:09:48 +0200

    add circle

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame
 import sys 
 
+class XO:
+    cross = pygame.image.load('cross.png')
+    circle = pygame.image.load('circle.png')
+    DEFAULT_IMAGE_SIZE = (50, 50)
+    cross = pygame.transform.scale(cross, DEFAULT_IMAGE_SIZE)
+    circle = pygame.transform.scale(circle, DEFAULT_IMAGE_SIZE)
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((600, 500))
     bg = pygame.image.load('background.png')
-    cross = pygame.image.load('cross.png')
+    x = XO.cross
+    o = XO.circle
     screen.blit(bg, (0, 0))
     pygame.display.flip()
+    counter = 0
     while 1:
         pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                screen.blit(cross, (pos))
-                pygame.display.flip()
-            elif event.type == pygame.MOUSEBUTTONUP:
-                pass
-
+                if (counter % 2) == 0:
+                    counter += 1
+                    screen.blit(o, (pos))
+                    pygame.display.flip()
+                else:
+                    counter +=1
+                    screen.blit(x, (pos))
+                    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
