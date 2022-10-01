34bf8bf5c1699274b05474c8cd00588988163dda
Jagoda Blaszkiewicz <jagabla8@gmail.com>
2022-10-02 01:21:08 +0200

    changes

--- main.py
+++ main.py
@@ -7,30 +7,35 @@ class XO:
     DEFAULT_IMAGE_SIZE = (50, 50)
     cross = pygame.transform.scale(cross, DEFAULT_IMAGE_SIZE)
     circle = pygame.transform.scale(circle, DEFAULT_IMAGE_SIZE)
+    def draw_x(self, screen):
+        pos = pygame.mouse.get_pos()
+        screen.blit(self.cross, (pos))
+        pygame.display.flip()
+    def draw_o(self, screen):
+        pos = pygame.mouse.get_pos()
+        screen.blit(self.circle, (pos))
+        pygame.display.flip()
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((600, 500))
     bg = pygame.image.load('background.png')
-    x = XO.cross
-    o = XO.circle
+    x = XO
+    o = XO
     screen.blit(bg, (0, 0))
     pygame.display.flip()
     counter = 0
     while 1:
-        pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if (counter % 2) == 0:
                     counter += 1
-                    screen.blit(o, (pos))
-                    pygame.display.flip()
+                    o.draw_o(o, screen)
                 else:
                     counter +=1
-                    screen.blit(x, (pos))
-                    pygame.display.flip()
+                    x.draw_x(x, screen)
 
 if __name__ == "__main__":
     main()
