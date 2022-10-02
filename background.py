3ccd2fe57b7e18a76a63794939a5bd92bff6caf8
Jagoda Blaszkiewicz <jagabla8@gmail.com>
2022-10-02 21:34:32 +0200

    class fix

--- background.py
+++ background.py
@@ -4,25 +4,23 @@ import random
 pygame.init()
 
 x = 600
+y = x/3
 screen = pygame.display.set_mode((x, x))
 background = pygame.Surface(screen.get_size())
 background = background.convert()
 background.fill((0, 0, 0))
 white = (220,220,220)
+list_rect=[]
 
 class CrissCross:
-    #each value represents different rectiangle, e.g lt = left top
-    lt = pygame.draw.rect(background, white, (0, 0, x/3, x/3))
-    mt = pygame.draw.rect(background, white, (x/3 +1, 0, x/3, x/3))
-    rt = pygame.draw.rect(background, white, (x/3*2 + 2 ,0, x/3, x/3))
-    ml = pygame.draw.rect(background, white, (0,  x/3 +1, x/3, x/3))
-    mm = pygame.draw.rect(background, white, (x/3 +1, x/3 +1, x/3, x/3))
-    mr = pygame.draw.rect(background, white, (x/3*2 + 2, x/3 +1, x/3, x/3))
-    dl = pygame.draw.rect(background, white, (0,  x/3*2 +2, x/3, x/3))
-    dm = pygame.draw.rect(background, white, (x/3 +1,  x/3*2 +2, x/3, x/3))
-    dr = pygame.draw.rect(background, white, (x/3*2 + 2,  x/3*2 +2, x/3, x/3))
-    list_rect = [lt,mt,rt,ml,mm,mr,dl,dm,dr]
-    # def draw_item(self, position):
+    def __init__(self):
+        for i in range(3):
+            for j in range(3):
+                rect = pygame.draw.rect(background, white, ((y+1)*i,(y+1)*j, y, y))
+                list_rect.append(rect)
+    def list_rect():
+        return list_rect
+tl = CrissCross()
 
 cross = pygame.Rect(200, 100, 161, 100)
 circle = pygame.Rect(200, 100, 161, 100)
@@ -32,18 +30,18 @@ while 1:
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            for rect in CrissCross.list_rect:
+            for rect in CrissCross.list_rect():
                 if pygame.Rect.collidepoint(rect, pygame.mouse.get_pos()):
                     cross.center = rect.center
-                    pygame.draw.rect(background, (0, 100, 255), cross, 2)
+                    pygame.draw.rect(background, (0, 100, 255), cross, 5)
                     pygame.display.flip()
-                    new_list = CrissCross.list_rect
+                    new_list = CrissCross.list_rect()
                     new_list.remove(rect)
                     random_rect = random.choice(new_list)
             for rect in new_list:
                 if pygame.Rect.colliderect(rect, random_rect):
                     circle.center = rect.center
-                    pygame.draw.rect(background, (0, 0, 0), circle, 2)
+                    pygame.draw.rect(background, (0, 0, 0), circle, 5)
                     pygame.display.flip()
                     new_list.remove(rect)
                     break
