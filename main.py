61020f844ad0fcd5d90416c2d40544113f561bdd
Jagoda Blaszkiewicz <jagabla8@gmail.com>
2022-10-04 21:18:44 +0200

    wider backgroud lines

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def create_list(background, y):
     list_rect = []
     for i in range(3):
         for j in range(3):
-            rect = pygame.draw.rect(background, (220,220,220), ((y+1)*i,(y+1)*j, y, y))
+            rect = pygame.draw.rect(background, (220,220,220), ((y+5)*i,(y+5)*j, y, y))
             list_rect.append(rect)
     return list_rect
 
@@ -14,7 +14,7 @@ def create_map(background, y):
     map = []
     for i in range(3):
         for j in range(3):
-            rect = pygame.draw.rect(background, (220,220,220), ((y+1)*i,(y+1)*j, y, y))
+            rect = pygame.draw.rect(background, (220,220,220), ((y+5)*i,(y+5)*j, y, y))
             map.append([rect, 0])
     return map
 
@@ -43,11 +43,11 @@ def is_winning(map):
 
 def draw_cross(background, center_point):
     pygame.draw.lines(background, (0,0,0), False, 
-    [[center_point[0] -45,center_point[1] -45], 
+    [[center_point[0]-45,center_point[1]-45], 
     center_point, 
-    [center_point[0] -45, center_point[1]+45], 
+    [center_point[0]-45, center_point[1]+45], 
     center_point, 
-    [center_point[0] +45, center_point[1]-45],
+    [center_point[0]+45, center_point[1]-45],
     center_point, 
     [center_point[0]+45, center_point[1]+45]], 8)
 
