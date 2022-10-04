8adf1c17b35a58f75f7e69aa9d7b91ba5461da28
Jagoda Blaszkiewicz <jagabla8@gmail.com>
2022-10-04 18:25:41 +0200

    fix functions

--- background.py
+++ background.py
@@ -1,39 +1,30 @@
 import pygame
 import sys
 import random
-pygame.init()
 
-x = 600
-y = x/3
-screen = pygame.display.set_mode((x, x))
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((0, 0, 0))
-white = (220,220,220)
+def create_list(background, white, y):
+    list_rect = []
+    for i in range(3):
+        for j in range(3):
+            rect = pygame.draw.rect(background, white, ((y+1)*i,(y+1)*j, y, y))
+            list_rect.append(rect)
+    return list_rect
 
-list_rect=[]
-map = []
+def create_map(background, white, y):
+    map = []
+    for i in range(3):
+        for j in range(3):
+            rect = pygame.draw.rect(background, white, ((y+1)*i,(y+1)*j, y, y))
+            map.append([rect, 0])
+    return map
 
-
-class CrissCross:
-    def __init__(self):
-        for i in range(3):
-            for j in range(3):
-                rect = pygame.draw.rect(background, white, ((y+1)*i,(y+1)*j, y, y))
-                list_rect.append(rect)
-                map.append([rect,0])
-    def list_rect():
-        return list_rect
-
-tl = CrissCross()
-
-def change_map_value(rect):
+def change_map_value(rect, map):
     for i in map:
         if i[0] == rect:
             i[1] = 1
     return map
 
-def is_winning():
+def is_winning(map):
     if map[0][1] == 1 and map[1][1]==1 and map [2][1] == 1:
         return True
     elif map[3][1] == 1 and map[4][1]==1 and map [5][1] == 1:
@@ -53,28 +44,42 @@ def is_winning():
 
 cross = pygame.Rect(200, 100, 161, 100)
 circle = pygame.Rect(200, 100, 161, 100)
-winner = []
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            for rect in CrissCross.list_rect():
-                if pygame.Rect.collidepoint(rect, pygame.mouse.get_pos()):
-                    cross.center = rect.center
-                    pygame.draw.rect(background, (0, 100, 255), cross, 5)
-                    change_map_value(rect)
-                    new_list = CrissCross.list_rect()
-                    new_list.remove(rect)
-                    random_rect = random.choice(new_list)
-                    if is_winning() == True:
-                        sys.exit()
-        elif event.type == pygame.MOUSEBUTTONUP:
-            for rect in new_list:
-                if pygame.Rect.colliderect(rect, random_rect):
-                    circle.center = rect.center
-                    pygame.draw.rect(background, (0, 0, 0), circle, 5)
-                    new_list.remove(rect)
-                    break
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
+
+def main():
+    pygame.init()
+    x = 600
+    y = x/3
+    screen = pygame.display.set_mode((x, x))
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0, 0, 0))
+    white = (220,220,220)
+    list_rect = create_list(background, white, y)
+    map = create_map(background, white, y)
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                for rect in list_rect:
+                    if pygame.Rect.collidepoint(rect, pygame.mouse.get_pos()):
+                        cross.center = rect.center
+                        pygame.draw.rect(background, (0, 100, 255), cross, 5)
+                        change_map_value(rect, map)
+                        new_list = list_rect
+                        new_list.remove(rect)
+                        random_rect = random.choice(new_list)
+                        if is_winning(map) == True:
+                            sys.exit()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                for rect in new_list:
+                    if pygame.Rect.colliderect(rect, random_rect):
+                        circle.center = rect.center
+                        pygame.draw.rect(background, (0, 0, 0), circle, 5)
+                        new_list.remove(rect)
+                        break
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
