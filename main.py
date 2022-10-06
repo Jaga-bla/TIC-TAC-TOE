cee3de333e68be6164de8edfe1178d9162632e6e
Jagoda Blaszkiewicz <jagabla8@gmail.com>
2022-10-06 21:07:14 +0200

    fix is_winning function

--- main.py
+++ main.py
@@ -18,52 +18,23 @@ def add_field_value(background, y) -> list:
             map.append([rect, 0])
     return map
 
-def change_map_value(rect, map):
+def change_field_value(rect, map, n):
     for i in map:
         if i[0] == rect:
-            i[1] = 1
+            i[1] = n
 
-def change_map_value_enemy(rect, map):
-    for i in map:
-        if i[0] == rect:
-            i[1] = -1
+def is_winning(map, n):
+    for j in range(0,7,3):
+        if map[0+j][1] ==n and map[1+j][1]==n and map [2+j][1] ==n:
+            return True
+    for j in range(3):
+        if map[0+j][1] ==n and map[3+j][1]==n and map [6+j][1] ==n:
+            return True
+    for j in range(0,3,2):
+        if map[0+j][1] ==n and map[4][1]==n and map [8-j][1] ==n:
+            return True
 
-def is_winning(map):
-    if map[0][1] ==1 and map[1][1]==1 and map [2][1] ==1:
-        return True
-    elif map[3][1] ==1 and map[4][1]==1 and map [5][1] ==1:
-        return True
-    elif map[6][1] ==1 and map[7][1]==1 and map [8][1] ==1:
-        return True
-    elif map[0][1] ==1 and map[3][1]==1 and map [6][1] ==1:
-        return True
-    elif map[1][1] ==1 and map[4][1]==1 and map [7][1] ==1:
-        return True
-    elif map[2][1] ==1 and map[5][1]==1 and map [8][1] ==1:
-        return True
-    elif map[0][1] ==1 and map[4][1]==1 and map [8][1] ==1:
-        return True
-    elif map[2][1] ==1 and map[4][1]==1 and map [6][1] ==1:
-        return True
-
-def is_loosing(map):
-    if map[0][1] ==-1 and map[1][1]==-1 and map [2][1] ==-1:
-        return True
-    elif map[3][1] ==-1 and map[4][1]==-1 and map [5][1] ==-1:
-        return True
-    elif map[6][1] ==-1 and map[7][1]==-1 and map [8][1] ==-1:
-        return True
-    elif map[0][1] ==-1 and map[3][1]==-1 and map [6][1] ==-1:
-        return True
-    elif map[1][1] ==-1 and map[4][1]==-1 and map [7][1] ==-1:
-        return True
-    elif map[2][1] ==-1 and map[5][1]==-1 and map [8][1] ==-1:
-        return True
-    elif map[0][1] ==-1 and map[4][1]==-1 and map [8][1] ==-1:
-        return True
-    elif map[2][1] ==-1 and map[4][1]==-1 and map [6][1] ==-1:
-        return True
-
+        
 def draw_cross(background, center_point):
     pygame.draw.lines(background, (0,0,0), False, 
     [[center_point[0]-45,center_point[1]-45], 
@@ -96,13 +67,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            if is_winning(map) == True:
+            if is_winning(map, 1) == True:
                 background.fill((220,220,220))
                 text = pygame.font.Font.render(pygame.font.SysFont("Dyuthi", 42), f'Congratulations, you won!', True, (0,0,0))
                 background.blit(text, (x/4, x/4))
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     list_rect, map, counter = initialize_background(background, y)
-            elif is_loosing(map) == True:
+            elif is_winning(map, -1) == True:
                 background.fill((220,220,220))
                 text = pygame.font.Font.render(pygame.font.SysFont("Dyuthi", 42), f'Your enemy won the game!', True, (0,0,0))
                 background.blit(text, (x/4, x/4))
@@ -123,7 +94,7 @@ def main():
                                     counter += 1
                                     center_point = rect.center
                                     draw_cross(background, center_point)
-                                    change_map_value(rect, map)
+                                    change_field_value(rect, map, 1)
                                     new_list = list_rect
                                     new_list.remove(rect)
                                     random_rect = random.choice(new_list)
@@ -134,7 +105,7 @@ def main():
                                     counter += 1
                                     center_point = rect.center
                                     draw_circle(background, center_point)
-                                    change_map_value_enemy(rect, map)
+                                    change_field_value(rect, map, -1)
                                     new_list.remove(rect)
                 except:
                     break   
