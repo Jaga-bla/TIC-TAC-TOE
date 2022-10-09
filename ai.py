85a8a672d8f4267ccf0f55f2e22cffaa5679fdaf
Jagoda Blaszkiewicz <jagabla8@gmail.com>
2022-10-09 19:43:57 +0200

    add comments

--- ai.py
+++ ai.py
@@ -1,31 +1,28 @@
 import random
 
 def choice_ai(map, empty_fields_list):
+    #prevent vertical win possibilities
     for j in range(0,7,3):
         if map[0+j][1] ==1 and map[1+j][1]==1 and map[2+j][1] ==0:
             return map[2+j][0]
+        if map[0+j][1] ==0 and map[1+j][1]==1 and map[2+j][1] ==1:
+            return map[0+j][0]
+        if map[0+j][1] ==1 and map[1+j][1]==0 and map[2+j][1] ==1:
+            return map[1+j][0]
+    #prevent horizontal win possibilities
     for j in range(3):
         if map[0+j][1] ==1 and map[3+j][1]==1 and map[6+j][1] ==0:
             return map[6+j][0]
+        if map[0+j][1] ==0 and map[3+j][1]==1 and map[6+j][1] ==1:
+            return map[0+j][0]
+        if map[0+j][1] ==1 and map[3+j][1]==0 and map[6+j][1] ==1:
+            return map[3+j][0]
+    #prevent diagonal win possibilities
     for j in range(0,3,2):
         if map[0+j][1] ==1 and map[4][1]==1 and map[8-j][1] ==0:
             return map[8-j][0]
-    for j in range(0,7,3):
-        if map[0+j][1] ==0 and map[1+j][1]==1 and map[2+j][1] ==1:
-            return map[0+j][0]
-    for j in range(3):
-        if map[0+j][1] ==0 and map[3+j][1]==1 and map[6+j][1] ==1:
-            return map[0+j][0]
-    for j in range(0,3,2):
         if map[0+j][1] ==0 and map[4][1]==1 and map[8-j][1] ==1:
             return map[0+j][0]
-    for j in range(0,7,3):
-        if map[0+j][1] ==1 and map[1+j][1]==0 and map[2+j][1] ==1:
-            return map[1+j][0]
-    for j in range(3):
-        if map[0+j][1] ==1 and map[3+j][1]==0 and map[6+j][1] ==1:
-            return map[3+j][0]
-    for j in range(0,3,2):
         if map[0+j][1] ==1 and map[4][1]==0 and map[8-j][1] ==1:
             return map[4][0]
     else:
--- game_items.py
+++ game_items.py
@@ -2,15 +2,24 @@ import pygame
 
 x = 600 #side lenght of the screen square
 y = x/3 #side lenght of the field square
+
+#create the background for all the components
 screen = pygame.display.set_mode((x, x))
 background = pygame.Surface(screen.get_size())
 background = background.convert()
+
+def draw_rect(i:int,j:int)->pygame.Rect:
+    color = (220,220,220)
+    position = ((y+5)*i,(y+5)*j)
+    size = (y,y)
+    rect = pygame.draw.rect(background, color, (position,size))
+    return rect
 
 def fields_list() -> list:
     list_rect = []
     for i in range(3):
         for j in range(3):
-            rect = pygame.draw.rect(background, (220,220,220), ((y+5)*i,(y+5)*j, y, y))
+            rect = draw_rect(i,j)
             list_rect.append(rect)
     return list_rect
 
@@ -18,7 +27,7 @@ def add_field_value() -> list:
     map = []
     for i in range(3):
         for j in range(3):
-            rect = pygame.draw.rect(background, (220,220,220), ((y+5)*i,(y+5)*j, y, y))
+            rect = draw_rect(i,j)
             map.append([rect, 0])
     return map
 
@@ -28,37 +37,45 @@ def change_field_value(rect:pygame.Rect, map:list, n:int):
             i[1] = n
 
 def is_winning(map:list, n:int)->bool:
+    #vertical win possibilities
     for j in range(0,7,3):
-        if map[0+j][1] ==n and map[1+j][1]==n and map[2+j][1] ==n:
+        if map[0+j][1] ==2 and map[1+j][1]==2 and map[2+j][1] ==2:
             return True
+    #horizontal win possibilities
     for j in range(3):
         if map[0+j][1] ==n and map[3+j][1]==n and map[6+j][1] ==n:
             return True
+    #diagonal win possibilities
     for j in range(0,3,2):
         if map[0+j][1] ==n and map[4][1]==n and map[8-j][1] ==n:
             return True
         
-def draw_cross(center_point:tuple)->pygame.Rect:
-    pygame.draw.lines(background, (0,0,0), False, 
-    [[center_point[0]-45,center_point[1]-45], 
+def draw_cross(center_point:tuple)-> pygame.Rect:
+    list_of_points = [[center_point[0]-45,center_point[1]-45], 
     center_point, 
     [center_point[0]-45, center_point[1]+45], 
     center_point, 
     [center_point[0]+45, center_point[1]-45],
     center_point, 
-    [center_point[0]+45, center_point[1]+45]], 8)
+    [center_point[0]+45, center_point[1]+45]]
+    color = (0,0,0)
+    joint_end_points = False
+    line_thickness = 8
+    cross = pygame.draw.lines(background, color, joint_end_points, list_of_points, line_thickness)
+    return cross
 
 def draw_circle(center_point:tuple)->pygame.Rect:
     pygame.draw.circle(background, (0,0,0), center_point, 45, 7)
 
-def initialize_background():
+def initialize_background()->[list,list, int]:
     background.fill((0, 0, 0))
     list_rect = fields_list()
     map = add_field_value()
     counter = 0
     return list_rect, map, counter 
 
-def message_screen(text:str):
+def message_screen(text:str) -> pygame:
     background.fill((220,220,220))
-    text = pygame.font.Font.render(pygame.font.SysFont("Dyuthi", 42), text, True, (0,0,0))
+    color = (0,0,0)
+    text = pygame.font.Font.render(pygame.font.SysFont("Dyuthi", 42), text, True, color)
     background.blit(text, (50, x/2))
